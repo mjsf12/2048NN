0c050a6e9127bc4cc65f8cf77963b5a4831227dc
Marcilio Junior <marciliojr.farias@gmail.com>
2018-09-28 13:55:12 -0300

    Rerodução feita

--- Tensor.py
+++ Tensor.py
@@ -3,6 +3,16 @@ from tensorflow import keras
 from tensorflow.python.keras.layers import Dense, Activation
 from tensorflow.python.keras.models import Sequential
 import numpy as np
+from time import sleep
+def mutacao(wid):
+    aux  = wid
+    while (True):
+        num = np.random.randint(len(aux))
+        if (not hasattr(aux[num], "__len__")):
+            aux[num] = np.random.uniform(-1,1)
+            break
+        aux = aux[num]
+    return wid
 y = []
 for x in range(16):
     if x==0:
@@ -14,9 +24,15 @@ y= np.matrix(y)
 model = Sequential()
 model.add(Dense(32, input_dim=16,kernel_initializer='random_uniform',bias_initializer='random_uniform'))
 model.add(Activation('hard_sigmoid'))
-model.add(Dense(4))
+model.add(Dense(4,kernel_initializer='random_uniform',bias_initializer='random_uniform'))
 model.add(Activation('softmax'))
-#print (model.get_weights())
+wid = model.get_weights()
 x = model.predict(y)
-print (x)
-print(x.argmax())
+while True:
+    print(model.predict(y))
+    wid = mutacao(wid);
+    model.set_weights(wid)
+    sleep(1)
+#print (aux)
+#print (wid)
+
